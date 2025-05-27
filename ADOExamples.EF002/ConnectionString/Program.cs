a58bb0b476f349fc22da299712b2104b6fbf8a07
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-05-27 21:51:02 +0300

    add ADOExamples.EF002

---
+++ ADOExamples.EF002/ConnectionString/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace ExecuteRawSql
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+
+            Console.WriteLine(configuration.GetSection("constr").Value);
+
+            Console.ReadKey();
+        }
+    }
+}
