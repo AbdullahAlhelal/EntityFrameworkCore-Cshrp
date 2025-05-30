e58bf0b63f0d73de6cf0aaca2ea6c12d619ffa94
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-05-30 22:41:14 +0300

    Reverse Engineering  (Scaffold)

---
+++ Reverse Engineering  (Scaffold)  EF-008/ReverseEngineering/Data/AppDbContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using EF008.ReverseEngineering.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF008.ReverseEngineering.Data;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Speaker> Speakers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        => optionsBuilder.UseSqlServer("Data Source =  = ABDULLAH\\MSSQLSERVER22 ; Initial Catalog = TechTalk ; Integrated Security = SSPI; TrustServerCertificate = True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Speaker>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Speakers__3214EC0736C723DA");
+
+            entity.Property(e => e.FirstName)
+                .IsRequired()
+                .HasMaxLength(25);
+            entity.Property(e => e.LastName)
+                .IsRequired()
+                .HasMaxLength(25);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
